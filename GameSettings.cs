d8ee04762a279e86184931d9e35dc0f9cec17d17
fabianmedrano <135065707+fabianmedrano@users.noreply.github.com>
2023-07-25 14:05:37 -0600

    Agregar archivos de proyecto.

---
+++ GameSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    public class GameSettings
+    {
+        private static GameSettings instance;
+        public int Difficultty { get; set; }
+        public int SoundVolume { get; set; }
+        public int ScreenResolution { get; set; }
+
+        private GameSettings() { 
+            Difficultty = 4;
+            SoundVolume = 9;
+            ScreenResolution = 1028;
+        }
+        public static GameSettings Instance
+        {
+            get { if (instance == null) { 
+                   instance = new GameSettings();   
+                }
+            return instance;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameSettings.Instance.Difficultty = 3;
+            GameSettings.Instance.SoundVolume = 1;
+            GameSettings.Instance.ScreenResolution = 720;
+        }
+    }
+}
